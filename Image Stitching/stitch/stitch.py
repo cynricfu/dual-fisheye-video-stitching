ab311241a36aba60ae4fab1d6d857c86cec06680
Cynric <fuxy203@163.com>
2017-07-20 15:36:20 +0800

    modify stitching script interface

--- Image Stitching/stitch/stitch.py
+++ Image Stitching/stitch/stitch.py
@@ -1,37 +1,57 @@
 #!/usr/bin/python
-# import the necessary packages
-from panorama import Stitcher
+from stitcher import Stitcher
 import argparse
-#import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--first", required=True,
-                help="path to the first image")
-ap.add_argument("-s", "--second", required=True,
-                help="path to the second image")
-args = vars(ap.parse_args())
 
-# load the two images and resize them to have a width of 400 pixels
-# (for faster processing)
-width = 200
-imageA = cv2.imread(args["first"])
-imageB = cv2.imread(args["second"])
-imageA = cv2.resize(
-    imageA, (width, int(imageA.shape[0] * float(width) / imageA.shape[1])))
-#imageA = imutils.resize(imageA, width=400)
-imageB = cv2.resize(
-    imageB, (width, int(imageB.shape[0] * float(width) / imageB.shape[1])))
-#imageB = imutils.resize(imageB, width=400)
+def stitch(img1, img2, sigma, levels, flag_fast):
+    if flag_fast:
+        if img1.shape[1] > 400:
+            img1 = cv2.resize(
+                img1, (width, int(img1.shape[0] * float(width) / img1.shape[1])))
+        if img2.shape[1] > 400:
+            img2 = cv2.resize(
+                img2, (width, int(img2.shape[0] * float(width) / img2.shape[1])))
 
-# stitch the images together to create a panorama
-stitcher = Stitcher()
-(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+    # stitch the images together to create a panorama
+    stitcher = Stitcher()
+    return stitcher.stitch([img1, img2], sigma=sigma, levels=levels,
+                           showMatches=True)
 
-# show the images
-cv2.imshow("Image A", imageA)
-cv2.imshow("Image B", imageB)
-cv2.imshow("Keypoint Matches", vis)
-cv2.imshow("Result", result)
-cv2.waitKey(0)
+
+if __name__ == '__main__':
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser(
+        description="A Python script for imgage stitching (panorama generating)")
+    ap.add_argument('-F', '--fast', required=False, action='store_true',
+                    help="option to resize images (to of width 400) for faster processing")
+    ap.add_argument('-f', '--first', required=True,
+                    help="path to the first image")
+    ap.add_argument('-s', '--second', required=True,
+                    help="path to the second image")
+    ap.add_argument('-S', '--sigma', required=False, type=float, default=2.0,
+                    help="standard deviation of Gaussian function, 2.0 by default")
+    ap.add_argument('-l', '--levels', required=False, type=int,
+                    help="number of levels of multi-band blending, calculated from image size if not provided")
+    args = vars(ap.parse_args())
+
+    # load the two images and other arguments
+    flag_fast = args['fast']
+    img1 = cv2.imread(args['first'])
+    img2 = cv2.imread(args['second'])
+    sigma = args['sigma']
+    levels = args['levels']
+
+    # stitch the two images
+    resvis = stitch(img1, img2, sigma, levels, flag_fast)
+
+    if resvis is None:
+        print "there aren't enough matched keypoints to create a panorama"
+    else:
+        # show the images
+        result, vis = resvis
+        cv2.imshow("Image A", img1)
+        cv2.imshow("Image B", img2)
+        cv2.imshow("Keypoint Matches", vis)
+        cv2.imshow("Result", result)
+        cv2.waitKey(0)
--- Image Stitching/stitch/stitcher.py
+++ Image Stitching/stitch/stitcher.py
@@ -1,15 +1,14 @@
-# import the necessary packages
 import numpy as np
-#import imutils
 import cv2
-from multi_band_blending import *
+from multi_band_blending import multi_band_blending
+
 
 class Stitcher:
     def __init__(self):
         print cv2.__version__
 
-    def stitch(self, images, ratio=0.75, reprojThresh=2.0,
-        showMatches=False):
+    def stitch(self, images, ratio=0.75, reprojThresh=2.0, sigma=2.0,
+               levels=None, showMatches=False):
         # unpack the images, then detect keypoints and extract
         # local invariant descriptors from them
         (imageA, imageB) = images
@@ -17,51 +16,49 @@ class Stitcher:
         (kpsB, featuresB) = self.detectAndDescribe(imageB)
 
         # match features between the two images
-        M = self.matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh)
+        M = self.matchKeypoints(kpsA, kpsB, featuresA,
+                                featuresB, ratio, reprojThresh)
 
         # if the match is None, then there aren't enough matched
         # keypoints to create a panorama
         if M is None:
             return None
 
-        # otherwise, apply a perspective warp to stitch the images
-        # together
+        # otherwise, apply a perspective warp to stitch the images together
         (matches, H, status) = M
-        #result = cv2.warpPerspective(imageB, H,
-        #    (imageA.shape[1] + imageB.shape[1], imageB.shape[0]))
-        #result[0:imageA.shape[0], 0:imageA.shape[1]] = imageA
-        subB = cv2.warpPerspective(imageB, H,
-            (imageA.shape[1] + imageB.shape[1], imageB.shape[0]))
-        subA = np.zeros((imageB.shape[0], imageA.shape[1] + imageB.shape[1],3))
+        subB = cv2.warpPerspective(
+            imageB, H, (imageA.shape[1] + imageB.shape[1], imageB.shape[0]))
+        subA = np.zeros(
+            (imageB.shape[0], imageA.shape[1] + imageB.shape[1], 3))
         subA[0:imageA.shape[0], 0:imageA.shape[1]] = imageA
 
-        result = multi_band_blending(subA, subB, 400)
+        # apply multi-band blending
+        result = multi_band_blending(
+            subA, subB, overlap_w=imageA.shape[1] + imageB.shape[1], sigma=sigma, levels=levels)
         result = result.astype(np.uint8)
 
         # check to see if the keypoint matches should be visualized
         if showMatches:
             vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches,
-                status)
-
-            # return a tuple of the stitched image and the
-            # visualization
+                                   status)
+            # return a tuple of the stitched image and the visualization
             return (result, vis)
-
-        # return the stitched image
-        return result
+        else:
+            # return the stitched image
+            return result
 
     def detectAndDescribe(self, image):
-        # convert the image to grayscale
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
         # check to see if we are using OpenCV 3.X
         if int(cv2.__version__[0]) >= 3:
-        	# detect and extract features from the image
+            # detect and extract features from the image
             descriptor = cv2.xfeatures2d.SIFT_create()
             (kps, features) = descriptor.detectAndCompute(image, None)
 
         # otherwise, we are using OpenCV 2.4.X
         else:
+            # convert the image to grayscale
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
             # detect keypoints in the image
             detector = cv2.FeatureDetector_create("SIFT")
             kps = detector.detect(gray)
@@ -70,43 +67,32 @@ class Stitcher:
             extractor = cv2.DescriptorExtractor_create("SIFT")
             (kps, features) = extractor.compute(gray, kps)
 
-        # convert the keypoints from KeyPoint objects to NumPy
-        # arrays
+        # convert the keypoints from KeyPoint objects to NumPy arrays
         kps = np.float32([kp.pt for kp in kps])
 
         # return a tuple of keypoints and features
         return (kps, features)
 
     def matchKeypoints(self, kpsA, kpsB, featuresA, featuresB,
-        ratio, reprojThresh):
+                       ratio, reprojThresh):
         # FLANN parameters
         FLANN_INDEX_KDTREE = 0
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)   # or pass empty dictionary
 
+        # compute the raw matches
         flann = cv2.FlannBasedMatcher(index_params, search_params)
-
         rawMatches = flann.knnMatch(featuresA, featuresB, k=2)
 
+        # perform Lowe's ratio test to get actual matches
         matches = []
-
         for m, n in rawMatches:
-            if m.distance < ratio * n.distance:
-                #if kpsA[m.queryIdx, 0] > 1130 and kpsA[m.queryIdx, 1] > 440 and kpsA[m.queryIdx, 1] < 840:
-                    matches.append((m.trainIdx, m.queryIdx))
-
-        # compute the raw matches and initialize the list of actual
-        # matches
-        #matcher = cv2.DescriptorMatcher_create("BruteForce")
-        #rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
-        #matches = []
-
-        # loop over the raw matches
-        #for m, n in rawMatches:
             # ensure the distance is within a certain ratio of each
             # other (i.e. Lowe's ratio test)
-            #if m.distance < n.distance * ratio:
-                #matches.append((m.trainIdx, m.queryIdx))
+            if m.distance < ratio * n.distance:
+                # here queryIdx corresponds to kpsA
+                # trainIdx corresponds to kpsB
+                matches.append((m.trainIdx, m.queryIdx))
 
         # computing a homography requires at least 4 matches
         if len(matches) > 4:
@@ -115,15 +101,15 @@ class Stitcher:
             ptsB = np.float32([kpsB[i] for (i, _) in matches])
 
             # compute the homography between the two sets of points
-            (H, status) = cv2.findHomography(ptsB, ptsA, cv2.RANSAC,
-                reprojThresh)
+            (H, status) = cv2.findHomography(
+                ptsB, ptsA, cv2.RANSAC, reprojThresh)
 
             # return the matches along with the homograpy matrix
             # and status of each matched point
             return (matches, H, status)
-
-        # otherwise, no homograpy could be computed
-        return None
+        else:
+            # otherwise, no homograpy could be computed
+            return None
 
     def drawMatches(self, imageA, imageB, kpsA, kpsB, matches, status):
         # initialize the output visualization image
